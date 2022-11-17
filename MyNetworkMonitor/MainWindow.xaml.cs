d067876ae84c29dfd5debcd1f95c6dd48b8e5bb5
Thomas Müller <Thomas.Mueller@tuta.io>
2022-11-17 18:15:45 +0100

    find the way

--- MyNetworkMonitor/MainWindow.xaml.cs
+++ MyNetworkMonitor/MainWindow.xaml.cs
@@ -1,6 +1,12 @@
-﻿using System;
+﻿using Rssdp;
+using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
 using System.Linq;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -15,14 +21,135 @@ using System.Windows.Shapes;
 
 namespace MyNetworkMonitor
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    // install as Service https://www.youtube.com/watch?v=y64L-3HKuP0
+
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            dgv_Results.ItemsSource = scanningMethods.NetworkResultsTable.DefaultView;
+        }
+
+        private void dgv_Devices_OnAutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
+        {
+            if (e.PropertyName == "Ping")
+            {
+                // replace text column with image column
+                e.Column = new DataGridTemplateColumn
+                {
+                    // searching for predefined tenplate in Resources
+                    CellTemplate = (sender as DataGrid).Resources["Ping"] as DataTemplate,
+                    HeaderTemplate = e.Column.HeaderTemplate,
+                    Header = e.Column.Header
+                };
+            }
+
+            if (e.PropertyName == "SSDP")
+            {
+                // replace text column with image column
+                e.Column = new DataGridTemplateColumn
+                {
+                    // searching for predefined tenplate in Resources
+                    CellTemplate = (sender as DataGrid).Resources["SSDP"] as DataTemplate,
+                    HeaderTemplate = e.Column.HeaderTemplate,
+                    Header = e.Column.Header
+                };
+            }
+        }
+
+        ScanningMethods_Ping scanningMethods = new ScanningMethods_Ping();
+
+
+        private void bt_Scan_IP_Ranges_Click(object sender, RoutedEventArgs e)
+        {
+            scanningMethods.CustomEvent_PingProgress += ScanningMethods_CustomEvent_PingProgress;
+            scanningMethods.CustomEvent_PingFinished += ScanningMethods_CustomEvent_PingFinished;
+
+            string myIP = scanningMethods.GetLocalIPv4(System.Net.NetworkInformation.NetworkInterfaceType.Ethernet);
+
+            myIP = "192.168.178.1";
+            myIP = String.Join(".", myIP.Split(".")[0], myIP.Split(".")[1], myIP.Split(".")[2], "{0}");
+
+            List<string> IPs = new List<string>();
+
+            for (int i = 1; i < 255; i++)
+            {
+                IPs.Add(string.Format(myIP, i));
+            }
+
+            scanningMethods.PingIPsAsync(IPs, null, 100, false);
+        }
+
+        private void ScanningMethods_CustomEvent_PingProgress(object? sender, PingProgressEventArgs e)
+        {
+            Dispatcher.BeginInvoke(() =>
+            {
+                //dt.Rows.Add(e.PingResultsRow);
+            });
+        }
+
+        private void ScanningMethods_CustomEvent_PingFinished(object? sender, PingFinishedEventArgs e)
+        {
+            Dispatcher.BeginInvoke(() =>
+            {
+                dgv_Results.ItemsSource = e.PingResults.DefaultView;
+            });
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //https://blog.noser.com/geraetesuche-mit-ssdp-in-net-im-lokalen-netzwerk/
+
+            //var ipAddresses = scanningMethods.NetworkResultsTable.AsEnumerable().Select(r => r.Field<string>("IPs")).ToList();
+
+            //foreach (var netIf in NetworkInterface.GetAllNetworkInterfaces())
+            //{
+            //    if (netIf.NetworkInterfaceType.Equals(NetworkInterfaceType.Ethernet) &&
+            //        netIf.OperationalStatus.Equals(OperationalStatus.Up) &&
+            //        !netIf.Name.Contains("vEthernet"))
+            //    {
+            //        foreach (var ip in netIf.GetIPProperties().UnicastAddresses)
+            //        {
+            //            if (ip.Address.AddressFamily.Equals(AddressFamily.InterNetwork))
+            //            {
+            //                var ipAddress = ip.Address.ToString();
+            //                Console.WriteLine($"{netIf.Name}: {ipAddress}'");
+            //                ipAddresses.Add(ipAddress);
+            //            }
+            //        }
+            //    }
+            //}
+
+            var searchTarget = "urn:schemas-upnp-org:device:WANDevice:1";
+            var devices = new ConcurrentBag<Discovered​Ssdp​Device>();
+
+                using (var deviceLocator = new SsdpDeviceLocator())
+                {
+                    var foundDevices =  await deviceLocator.SearchAsync();
+
+                    foreach (var foundDevice in foundDevices)
+                    {
+                        if(scanningMethods.NetworkResultsTable.AsEnumerable().Where(c => c.Field<string>("IP").Equals(foundDevice.DescriptionLocation.Host)).Count() == 0)
+                        {
+                            DataRow row = scanningMethods.NetworkResultsTable.NewRow();
+
+                            row["SendAlert"] = false;
+                            row["SSDP"] = Properties.Resources.green_dot;
+                            row["IP"] = foundDevice.DescriptionLocation.Host;
+                            row["ResponseTime"] = "";
+                            scanningMethods.NetworkResultsTable.Rows.Add(row);
+                        }
+                        else
+                        {
+                            //wenn bereits vorhanden dann setze SSDP auf green_dot
+                        }
+                        //Debug.WriteLine($"Device: IP={foundDevice.DescriptionLocation.Host}");
+                        //Debug.WriteLine($"Device: usn={foundDevice.Usn}");
+                        //devices.Add(foundDevice);
+                    }
+                }
         }
     }
 }
---
+++ MyNetworkMonitor/ScanningMethod_Ping.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Buffers.Text;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace MyNetworkMonitor
+{
+    internal class ScanningMethods_Ping
+    {
+        public ScanningMethods_Ping()
+        {
+            dt_NetworkResults.Columns.Add("SSDP", typeof(byte[]));
+            dt_NetworkResults.Columns.Add("Ping", typeof(byte[]));
+            dt_NetworkResults.Columns.Add("SendAlert", typeof(bool));
+            dt_NetworkResults.Columns.Add("IP", typeof(string));
+            dt_NetworkResults.Columns.Add("Hostname", typeof(string));
+            dt_NetworkResults.Columns.Add("ResponseTime", typeof(string));
+            dt_NetworkResults.Columns.Add("Ports", typeof(string));
+            dt_NetworkResults.Columns.Add("Comment", typeof(string));
+            dt_NetworkResults.Columns.Add("Mac", typeof(string));
+            dt_NetworkResults.Columns.Add("Vendor", typeof(string));
+            dt_NetworkResults.Columns.Add("Exception", typeof(string));            
+
+        }
+
+        // in .Net Core above need the Install of this nuget Package: Install-Package System.Drawing.Common
+  
+
+        DataTable dt_NetworkResults = new DataTable();
+
+        List<PingReply> pingReply = new List<PingReply>();
+
+        public event EventHandler<PingFinishedEventArgs>? CustomEvent_PingFinished;
+        public event EventHandler<PingProgressEventArgs>? CustomEvent_PingProgress;
+
+        private List<PingReply> PingResults
+        {
+            get { return pingReply; }
+        }
+
+        public DataTable NetworkResultsTable
+        {
+            get { return dt_NetworkResults; }
+        }
+
+        /// <summary>
+        /// to get the Ping result call the Propertie PingResults
+        /// </summary>
+        /// <param name="IPs"></param>
+        /// <param name="DNS_Server_IP"></param>
+        public async void PingIPsAsync(List<string> IPs, string DNS_Server_IP, int Timeout, bool ShowUnused = true)
+        {
+            var tasks = new List<Task>();
+
+            foreach (var ip in IPs)
+            {
+                System.Net.NetworkInformation.Ping p = new System.Net.NetworkInformation.Ping();
+                var task = PingAndUpdateAsync(p, ip, Timeout, ShowUnused);
+                tasks.Add(task);
+            }           
+            await Task.WhenAll(tasks);
+
+            if (CustomEvent_PingFinished != null)
+            {
+                //the User Gui can be freeze if a event fires to fast
+                CustomEvent_PingFinished(this, new PingFinishedEventArgs(dt_NetworkResults));
+            }
+        }
+
+        private async Task PingAndUpdateAsync(System.Net.NetworkInformation.Ping ping, string ip, int TimeOut, bool ShowUnused)
+        {
+            PingReply reply = await ping.SendPingAsync(ip, TimeOut);
+            DataRow row = dt_NetworkResults.NewRow();
+
+            row["SendAlert"] = false;
+
+            if (reply.Status == IPStatus.Success)
+            {
+                pingReply.Add(reply);
+
+                //row["SSDP"] = null;
+                row["Ping"] = Properties.Resources.green_dot;
+                row["IP"] = reply.Address.ToString();
+                row["ResponseTime"] = reply.RoundtripTime.ToString();
+                dt_NetworkResults.Rows.Add(row);
+            }
+            else if (ShowUnused && reply.Status != IPStatus.Success)
+            {
+                //row["SSDP"] = null;
+                row["Ping"] = Properties.Resources.red_dot;
+                row["IP"] = ip;
+                row["ResponseTime"] = string.Empty;
+                dt_NetworkResults.Rows.Add(row);
+            }
+
+            if (CustomEvent_PingFinished != null)
+            {
+                //the User Gui can be freeze if a event fires to fast
+                CustomEvent_PingProgress(this, new PingProgressEventArgs(row));
+            }
+        }
+
+
+
+        private bool IsPortOpen(string host_or_ip, int port, TimeSpan timeout)
+        {
+            try
+            {
+                using (var client = new TcpClient())
+                {
+                    var result = client.BeginConnect(host_or_ip, port, null, null);
+                    var success = result.AsyncWaitHandle.WaitOne(timeout);
+                    client.EndConnect(result);
+                    return success;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public string GetLocalIPv4(NetworkInterfaceType _type)
+        {
+            string output = "";
+            foreach (NetworkInterface item in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (item.NetworkInterfaceType == _type && item.OperationalStatus == OperationalStatus.Up)
+                {
+                    foreach (UnicastIPAddressInformation ip in item.GetIPProperties().UnicastAddresses)
+                    {
+                        if (ip.Address.AddressFamily == AddressFamily.InterNetwork)
+                        {
+                            output = ip.Address.ToString();
+                        }
+                    }
+                }
+            }
+            return output;
+        }
+    }
+
+
+    /* ############# Events #################*/
+
+    public class PingFinishedEventArgs : EventArgs
+    {
+        private DataTable _dt = new DataTable();
+        public DataTable PingResults { get { return _dt; } }
+        public PingFinishedEventArgs(DataTable PingResults)
+        {
+            _dt = PingResults;
+        }
+    }
+
+
+    public class PingProgressEventArgs : EventArgs
+    {
+        private DataRow _row = new ScanningMethods_Ping().NetworkResultsTable.NewRow();
+        public DataRow PingResultsRow { get { return _row; } }
+        public PingProgressEventArgs(DataRow Row)
+        {
+            _row = Row;
+        }
+    }
+}
