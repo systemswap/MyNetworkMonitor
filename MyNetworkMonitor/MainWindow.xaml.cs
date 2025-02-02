8dc3d47de7a9b334d5b7366167873cbffb73dc17
Thomas <Thomas-PC>
2025-02-02 20:54:33 +0100

    Service scan optimiert

--- MyNetworkMonitor/MainWindow.xaml.cs
+++ MyNetworkMonitor/MainWindow.xaml.cs
@@ -957,7 +957,8 @@ namespace MyNetworkMonitor
                 //};
 
                 status_Services_Scan = ScanStatus.running;
-                await scanningMethod_Services.ScanIPsAsync(Services_IPsToScan, new List<ServiceType> {ServiceType.RDP, ServiceType.UltraVNC, ServiceType.Anydesk, ServiceType.OPCUA, ServiceType.OPCDA, ServiceType.Teamviewer });//ServiceType.BigFixRemote, ServiceType.RDP, ServiceType.UltraVNC, ServiceType.Teamviewer, ServiceType.Anydesk, ServiceType.MSSQLServer,  ServiceType.OPCUA, ServiceType.OPCDA }, additionalServicePorts);
+                //await scanningMethod_Services.ScanIPsAsync(Services_IPsToScan, new List<ServiceType> {ServiceType.RDP, ServiceType.UltraVNC, ServiceType.Anydesk, ServiceType.OPCUA, ServiceType.OPCDA, ServiceType.Teamviewer });//ServiceType.BigFixRemote, ServiceType.RDP, ServiceType.UltraVNC, ServiceType.Teamviewer, ServiceType.Anydesk, ServiceType.MSSQLServer,  ServiceType.OPCUA, ServiceType.OPCDA }, additionalServicePorts);
+                await scanningMethod_Services.ScanIPsAsync(Services_IPsToScan, new List<ServiceType> { ServiceType.RDP, ServiceType.UltraVNC, ServiceType.Anydesk, ServiceType.OPCUA, ServiceType.OPCDA, ServiceType.Teamviewer });
             }
 
             if ((bool)chk_Methodes_LookUp.IsChecked)
