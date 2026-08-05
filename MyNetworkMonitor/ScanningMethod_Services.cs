a47a49748eef38a98bbe1eb26a65fc3b3814f345
Thomas Mueller <thomas.mueller@tuta.io>
2026-08-05 08:46:46 +0200

    Cleanup: ungenutztes using System.Printing aus Services entfernt
    System.Printing (ReachFramework, Windows-only) war in ScanningMethod_Services
    komplett ungenutzt - einziger "Print"-Bezug in der Datei. Entfernt, damit die
    Datei keine unnoetige Windows-Abhaengigkeit mehr traegt.

--- MyNetworkMonitor/ScanningMethod_Services.cs
+++ MyNetworkMonitor/ScanningMethod_Services.cs
@@ -14,7 +14,6 @@ using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
 using System.IO;
 using System.Collections.Concurrent;
-using System.Printing;
 using System.Reflection.Metadata;
 using System.Diagnostics;
 using System.Net.NetworkInformation;
